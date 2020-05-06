b68ac79c4751cf95445b21d09750993f475f2dfa
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-05-06 18:09:05 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>T.AL.K Family Planning Solution!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
