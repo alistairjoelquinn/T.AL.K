a3a021cb0507c924ae072104dfcec66aa916c90c
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-05-11 17:57:36 +0200

    tab navigator imported into app for talk to load

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import TalkNavigator from './navigation/TalkNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>T.AL.K Family Planning Solution!</Text>
-    </View>
+    <TalkNavigator />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
