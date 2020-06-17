e7580d04b05ecbde9fd0a4f9dc15984a4bd4c482
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-06-17 15:42:21 +0200

    diary input screen has selected date as header for input page

--- screens/DiaryInputScreen.js
+++ screens/DiaryInputScreen.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const DiaryInputScren = props => {
     const day = props.navigation.getParam('currentDay');
+    console.log('day: ', day);
+
+    const { navigation } = props;
+    useEffect(() => {
+        navigation.setParams({today: day});
+    }, [day]);
 
     return (
         <View style={styles.screen}>
-            <Text style={styles.text}>{day}</Text>
+            <Text style={styles.text}>Diary Input</Text>
         </View>
     );
 }
 
-DiaryInputScren.navigationOptions = {
-    headerTitle: 'Input Something New'
+DiaryInputScren.navigationOptions = navData => {
+    return {
+        headerTitle: navData.navigation.getParam('today')
+    }
 }
 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#1c1c1c'
     },
     text: {
-        color: '#d2d2d2'
+        color: '#d2d2d2',
+        fontSize: 22
     }
 });
 
