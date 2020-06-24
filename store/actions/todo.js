ef27389f0cc5964cc1259adfdcfafbf8bd0ae595
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-06-24 17:51:35 +0200

    todo items stored in db

--- store/actions/todo.js
+++ store/actions/todo.js
@@ -2,7 +2,18 @@ export const ADD_TO_DO = 'ADD_TO_DO';
 export const REMOVE_TO_DO = 'REMOVE_TO_DO';
 
 export const addToDoItem = item => {
-    return dispatch => {
+    return async dispatch => {
+        const response = await fetch('https://talk-app-e6de0.firebaseio.com/todo.json', {
+            method: 'POST',
+            header: {
+                'Content-Type': 'application.json'
+            },
+            body: JSON.stringify({
+                item
+            })
+        });
+    const resData = await response.json();
+    console.log('resData: ', resData);
         dispatch({
             type: ADD_TO_DO,
             item
