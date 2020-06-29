2aa2297dbec5a92c14434b0bc01e2dfbd3838525
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-06-29 10:45:13 +0200

    login styling component added for card style look upon login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { StyleSheet, ScrollView, View, KeyboardAvoidingView, Button, ActivityIndicator, Alert } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import Input from '../../components/Input';
 
 const LoginScreen = props => {
     return (
