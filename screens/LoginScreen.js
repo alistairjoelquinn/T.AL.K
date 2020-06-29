a6f616ccde2dedaf6cf7623db423b3b9ca7a164f
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-06-29 10:42:19 +0200

    Input component added for generic input validation

---
+++ screens/LoginScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, ScrollView, View, KeyboardAvoidingView, Button, ActivityIndicator, Alert } from 'react-native';
+
+const LoginScreen = props => {
+    return (
+
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default LoginScreen;
