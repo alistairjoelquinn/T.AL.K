0f98fe57d5d6e734cc0e8122862f009caa93e42b
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-07-15 10:32:17 +0200

    calendar reducer completed

--- store/reducers/calendar.js
+++ store/reducers/calendar.js
@@ -1,7 +1,35 @@
+import { ADD_CALENDAR_ITEM, REMOVE_CALENDAR_ITEM, SET_CALENDAR } from "../actions/calendar";
+
 const initialState = {
     calendarData: []
 };
 
 export default (state = initialState, action) => {
+    switch (action.type) {
+        case SET_CALENDAR:
+            return {
+                calendarData: action.list
+            };
+        case ADD_CALENDAR_ITEM:
+            return { 
+                ...state, 
+                calendarData: [ 
+                    {
+                        item: action.item,
+                        key: action.key
+                    },
+                    ...state.calendarData
+                ]
+            };
+        case REMOVE_CALENDAR_ITEM:
+            let shorter = state.calendarData;
+            let newList = shorter.filter(listItem => {
+                return listItem.key !== action.item;
+            });
+            return {
+                ...state,
+                calendarData: newList
+            };
+    }
     return state;
 };
