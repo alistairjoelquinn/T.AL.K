bdb503183c780680c28712f794dc9f86ff7fddc9
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-07-15 11:30:36 +0200

    double tap navigates user to new window while single tap should scroll but doesn't work

--- components/SingleEntry.js
+++ components/SingleEntry.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import DoubleClick from 'react-native-double-tap';
+
+
 import Colors from '../constants/Colors';
 
 const SingleEntry = props => {
@@ -8,38 +11,43 @@ const SingleEntry = props => {
     );
 
     return (
-        <TouchableOpacity onPress={() => {
-            props.navigation.navigate('DiaryInput', {
-                currentDay: props.dayInput,
-                currentPerson: props.personInput
-            });
-        }}>
-            <View style={{ 
-                ...styles.item,  
-                backgroundColor: Colors.paleText,
-                borderWidth: 3,
-                borderColor: (props.colorDayCalc === 'Saturday' || props.colorDayCalc === 'Sunday')
-                    ? 
-                        '#0048a1'
-                    : 
-                        ''
-            }}>
-                <Text style={styles.text}>
-                    {
-                        (props.day === 'Today' || props.day === 'Tomorrow') ?
-                        props.day :
-                        props.day.slice(0, 3)
+        <ScrollView>
+            <DoubleClick
+                doubleTap={() => {
+                    props.navigation.navigate('DiaryInput', {
+                        currentDay: props.dayInput,
+                        currentPerson: props.personInput
+                    });
+                }}
+                delay={200}
+            >
+                <View style={{ 
+                    ...styles.item,  
+                    backgroundColor: Colors.paleText,
+                    borderWidth: 3,
+                    borderColor: (props.colorDayCalc === 'Saturday' || props.colorDayCalc === 'Sunday')
+                        ? 
+                            '#0048a1'
+                        : 
+                            ''
+                }}>
+                    <Text style={styles.text}>
+                        {
+                            (props.day === 'Today' || props.day === 'Tomorrow') ?
+                            props.day :
+                            props.day.slice(0, 3)
+                        }
+                    </Text>
+                    {activitiesToday &&
+                        <View style={styles.scroller}>
+                            {activitiesToday.map(activity => {
+                                return <Text style={styles.activityText}>{activity.item.time} {activity.item.activity}</Text>
+                            })}
+                        </View>
                     }
-                </Text>
-                {activitiesToday &&
-                    <ScrollView style={styles.scroller}>
-                        {activitiesToday.map(activity => {
-                            return <Text style={styles.activityText}>{activity.item.time} {activity.item.activity}</Text>
-                        })}
-                    </ScrollView>
-                }
-            </View>
-        </TouchableOpacity>
+                </View>
+            </DoubleClick>
+        </ScrollView>
     );
 };
 
